a9ce5024478848eb34348e815790b78ee3421e40
Dagmar Velez <angelfalls48@gmail.com>
2013-08-26 12:26:45 -0700

    Update application.js

--- application.js
+++ application.js
@@ -1,6 +1,17 @@
+$(document).ready(function()){
+	
+$(#guess-bar).
+
+var randomnumber = Math.floor( Math.random()*101);
+
 var answer = prompt("How many balls do I have left?");
 if(answer= 78)
-	alert ("Game, Set, Match");
-	background-color: blue;
-else
-	alert ("Nope, sorry!");
+
+	alert ("Game, Set, Match. Good job!");
+	
+else if(isNaN(guess) == true || guess > 100 || guess < 0 || guess === '')
+	alert ("Please enter a number between 0 and 100.");
+	
+	
+	
+};
