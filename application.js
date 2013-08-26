74412644145b04ff3f1632b086e428434821b7cd
Dagmar Velez <angelfalls48@gmail.com>
2013-08-26 16:39:23 -0700

    Update application.js

--- application.js
+++ application.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
 	
-$(".button").on("mouseenter", function(){
-	$("h1").hide();
-});
-var randomnumber = Math.floor( Math.random()*101);
+//generates a number between 0 and 100
+var answer = Math.floor( Math.random()*101);
 
-var answer = prompt("How many balls do I have left?");
-if(answer= 78)
-
-	alert ("Game, Set, Match. Good job!");
+var guess = $("#guess").val();
+if $((guess-answer) <= Math.abs(5)){
+	return "Good job!";
+}
+else {
+	return "Not quite!";
+}
 	
-else if(isNaN(guess) == true || guess > 100 || guess < 0 || guess === '')
-	alert ("Please enter a number between 0 and 100.");
+//else if(isNaN(guess) === true || guess > 100 || guess < 0 || guess === '')
+	//alert ("Please enter a number between 0 and 100.");
 	
 	
 	
