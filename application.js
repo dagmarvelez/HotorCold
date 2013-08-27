da3ac2d590ec7191b0e633bdb59890dc46f2df8d
Dagmar Velez <angelfalls48@gmail.com>
2013-08-27 09:54:48 -0700

    Update application.js

--- application.js
+++ application.js
@@ -3,17 +3,30 @@ $(document).ready(function(){
 //generates a number between 0 and 100
 var answer = Math.floor( Math.random()*101);
 
-var guess = $("#guess").val();
-if $((guess-answer) <= Math.abs(5)){
-	return "Good job!";
-}
-else {
-	return "Not quite!";
+//submits answer when clicking the guess button
+function submitGuess(){
+	$("#guess-button").click(submit);
 }
 	
-//else if(isNaN(guess) === true || guess > 100 || guess < 0 || guess === '')
-	//alert ("Please enter a number between 0 and 100.");
-	
-	
+submitGuess();
+
+function submit(){
+	var guess = $("#guess").val();
+	if (guess == answer){
+		console.log("Congrats, you win!")
+	}
+	else if((guess-answer) <= Math.abs(5)){
+		console.log("You're getting warmer!");
+	}
+	else if ((guess-answer) > Math.abs(5)){
+		console.log("Not quite!");
+	}
+	else if ((guess-answer) > Math.abs(20)){
+		console.log("You're getting colder!");
+	}
+	else if(isNaN(guess) === true || guess > 100 || guess < 0 || guess === ''){
+		alert ("Please enter a number between 0 and 100.");
+	}
+}	
 	
 });
