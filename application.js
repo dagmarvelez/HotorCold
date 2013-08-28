6bff03fa9fed79567017714284c93532fa0153f2
Dagmar Velez <angelfalls48@gmail.com>
2013-08-28 12:28:20 -0700

    Update application.js

--- application.js
+++ application.js
@@ -6,19 +6,24 @@ var answer = Math.floor( Math.random()*101);
 var guess = $("#guess").val();
 
 function(submitGuess){
-	if (guess == answer)
+	if (guess == answer){
 		$("#win").addClass("visible-comment");
-	else if (Math.abs(guess-answer) <= 15)
+	}
+	else if (Math.abs(guess-answer) <= 15){
 		$("#warm").addClass("visible-comment");
-	else if (Math.abs(guess-answer) > 15)
+	}
+	else if (Math.abs(guess-answer) > 15){
 		$("#cold").addClass("visible-comment");
-	else if(guess > 100 || guess < 0 || isNaN(guess)===true)
+	}
+	else if(guess > 100 && guess < 0 && isNaN(guess)===true){
 		$("#error").addClass("visible-comment");
-}
+	}
+};
 
 $("#guess-button").click(submitGuess);
 if(enter.keyCode == 13){
 	submitGuess();
+	alert("Yes!");
 }
 //inputscope
 	
