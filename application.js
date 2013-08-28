5023715644254fe0f9c7cb0c5156831ff1ef104b
Dagmar Velez <angelfalls48@gmail.com>
2013-08-28 12:09:55 -0700

    Update application.js

--- application.js
+++ application.js
@@ -3,30 +3,28 @@ $(document).ready(function(){
 //generates a number between 0 and 100
 var answer = Math.floor( Math.random()*101);
 
-//submits answer when clicking the guess button
-function submitGuess(){
-	$("#guess-button").click(submit);
-}
-	
-submitGuess();
+var guess = $("#guess").val();
 
-function submit(){
-	var guess = $("#guess").val();
-	if (guess == answer){
-		console.log("Congrats, you win!")
-	}
-	else if((guess-answer) <= Math.abs(5)){
-		console.log("You're getting warmer!");
-	}
-	else if ((guess-answer) > Math.abs(5)){
-		console.log("Not quite!");
-	}
-	else if ((guess-answer) > Math.abs(20)){
-		console.log("You're getting colder!");
-	}
-	//else if(isNaN(guess) === true && guess > 100 && guess < 0 && guess === ''){
-		//alert ("Please enter a number between 0 and 100.");
-	//}
-}	
+function(submitGuess){
+	if (guess == answer)
+		$("#win").addClass("visible-comment");
+	else if (Math.abs(guess-answer) <= 15)
+		$("#warm").addClass("visible-comment");
+	else if (Math.abs(guess-answer) > 15)
+		$("#cold").addClass("visible-comment");
+	else if(guess > 100 || guess < 0 || isNaN(guess)===true)
+		$("#error").addClass("visible-comment");
+}
+
+$("#guess-button").click(submitGuess);
+if(enter.keyCode == 13){
+	submitGuess();
+}
+//inputscope
+	
+//else if(isNaN(guess) === true || guess > 100 || guess < 0 || guess === '')
+	//alert ("Please enter a number between 0 and 100.");
+	
+	
 	
 });
