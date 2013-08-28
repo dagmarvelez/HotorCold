a15f4ea11a5749c32bcb952edb5814c559b9fc16
Dagmar Velez <angelfalls48@gmail.com>
2013-08-28 12:48:38 -0700

    Update application.js

--- application.js
+++ application.js
@@ -1,31 +1,26 @@
 $(document).ready(function(){
 	
-//generates a number between 0 and 100
-var answer = Math.floor( Math.random()*101);
 
 function(submitGuess){
+	
+	var answer = Math.floor(Math.random()*101);
+	
 	var guess = $("#guess").val();
+	
 	if (guess == answer)
 		$("#win").addClass("visible-comment");
 	else if (Math.abs(guess-answer) <= 15)
 		$("#warm").addClass("visible-comment");
 	else if (Math.abs(guess-answer) > 15)
 		$("#cold").addClass("visible-comment");
-	else if(guess > 100 && guess < 0 && isNaN(guess)===true)
+	else if(guess > 100 && guess < 0 && isNaN(guess)===true){
 		$("#error").addClass("visible-comment");
 };
 
-$("#guess-button").on("click", function(){
-	submitGuess();
-}
+$("#guess-button").click(submitGuess);
 if(enter.keyCode == 13){
 	submitGuess();
-	alert("Yes!");
 }
-//inputscope
-	
-//else if(isNaN(guess) === true || guess > 100 || guess < 0 || guess === '')
-	//alert ("Please enter a number between 0 and 100.");
 	
 	
 	
