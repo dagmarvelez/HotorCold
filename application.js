2089e3e747bac95b56db86f738cc9b53581d3a73
Dagmar Velez <angelfalls48@gmail.com>
2013-08-29 08:55:50 -0700

    Update application.js

--- application.js
+++ application.js
@@ -1,27 +1,39 @@
 $(document).ready(function(){
+
+var answer = generateNewAnswer();
+
+function generateNewAnswer()){
 	
+$("#guess-button").on("click", function(event){
+//if(enter.keyCode == 13){
+	submitGuess(event);	
+}
+function generateNewAnswer(){
+	return Math.floor(Math.random()*101);
+}
 
 function(submitGuess){
 	
-	var answer = Math.floor(Math.random()*101);
-	
 	var guess = $("#guess").val();
 	
-	if (guess == answer)
+	if (guess == answer){
 		$("#win").addClass("visible-comment");
-	else if (Math.abs(guess-answer) <= 15)
-		$("#warm").addClass("visible-comment");
-	else if (Math.abs(guess-answer) > 15)
+	}
+	else if (Math.abs(guess-answer) <= 15){
+		$("#warm").addClass("visible-comment")
+		};
+	else if (Math.abs(guess-answer) > 15){
 		$("#cold").addClass("visible-comment");
+	}
 	else if(guess > 100 && guess < 0 && isNaN(guess)===true){
 		$("#error").addClass("visible-comment");
-};
+	}
 
-$("#guess-button").click(submitGuess);
-if(enter.keyCode == 13){
-	submitGuess();
-}
-	
-	
+
+
 	
 });
+	
+});
+
+};
